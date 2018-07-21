9d9c284dd9b048cfae7f754e46be388ad447f15e
clevercodinginnuendo <joshi.springer@gmail.com>
2018-07-21 16:02:05 +0200

    First commit
    First commit

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -10,6 +10,23 @@ namespace TextRPG
     {
         static void Main(string[] args)
         {
+            int selection = showCityScreen();
+            Console.WriteLine(selection);
+            Console.ReadLine();
+            
+        }
+
+        static int showCityScreen()
+        {
+            Console.WriteLine("Du bist in der Stadt");
+            Console.WriteLine("Es gibt:");
+            Console.WriteLine("1: Shop");
+            Console.WriteLine("2: Gasthaus");
+            Console.WriteLine("3: Stadt verlassen");
+
+        
+            return Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
